70cb96fcf1c0806cac37892117a2c12acd0bf7ea
Nathan Andrade <42661561+andradenathan@users.noreply.github.com>
2021-01-24 23:00:32 -0300

    + Exercício Lab7

--- lab7/lab7MT/e03.py
+++ lab7/lab7MT/e03.py
@@ -6,3 +6,17 @@ def posLetra(string, letra, numero):
     :param numero: int -> int
     :return: int -> int
     """
+    contador = 0
+    indice = 0
+    ocorrencia = 0
+    lista = list(string)
+    while contador < len(lista):
+        if lista[indice] == letra:
+            ocorrencia += 1
+            if ocorrencia == numero:
+                return indice
+
+        contador += 1
+        indice += 1
+
+    return -1
